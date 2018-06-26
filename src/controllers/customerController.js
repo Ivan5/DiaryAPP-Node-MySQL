a4864190c8bd2b1e29da61df56c286ec285f1e90
Ivan5 <converse_ivan@hotmail.com>
2018-06-26 14:01:31 -0500

    Add view and function to update

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -27,7 +27,28 @@ controller.add = (req,res)=>{
   });
 }
 controller.edit = (req,res)=>{
+  const id = req.params.id;
+  req.getConnection((err,conn)=>{
+    conn.query('select * from customer where id = ?',[id],(err,customer)=>{
+      res.render('customer_edit',{
+        data:customer[0]
+      })
+    })
+  })
+}
 
+controller.update = (req,res)=>{
+    const id = req.params.id;
+    /*const name = req.body.name;
+    const address = req.body.address;
+    const phone = req.body.phone;*/
+    const newCustomer = req.body;
+
+    req.getConnection((err,conn)=>{
+      conn.query('update customer set ? where id = ?',[newCustomer,id],(err,rows)=>{
+        res.redirect('/');
+      })
+    })
 }
 
 controller.delete = (req,res)=>{
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -6,4 +6,7 @@ router.get('/',customerController.list);
 
 router.post('/add',customerController.add);
 router.get('/delete/:id',customerController.delete);
+router.get('/update/:id',customerController.edit);
+router.post('/update/:id',customerController.update);
+
 module.exports = router;
