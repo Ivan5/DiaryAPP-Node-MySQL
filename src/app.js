f6c2147ce83e09de7ffdff80c9df7f4964494f12
Ivan5 <converse_ivan@hotmail.com>
2018-06-25 22:43:16 -0500

    Add basic config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 //Settings
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
 
 app.listen(app.get('port'),()=>{
   console.log('Server on port: ',app.get('port'));
-})
+});
